b80c8352e759a6d356f3748f643c99498f08302a
bullinnyc <i8u11@icloud.com>
2022-02-01 14:17:49 +0700

    Initial Commit

---
+++ GitHubRepositories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubRepositories
+//
+//  Created by Dmitry Kononchuk on 01.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepositories/GitHubRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesApp.swift
+//  GitHubRepositories
+//
+//  Created by Dmitry Kononchuk on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
